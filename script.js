9e8eadd985b2c36cd31ae9436e987915c39505fd
THESUSSLAYER <kartiksable8@gmail.com>
2024-11-02 23:25:18 +0530

    x tweet dialogue box created

---
+++ script.js
@@ -0,0 +1,11 @@
+function opentweet() 
+{
+    const overlayer = document.getElementById("overlay1");
+    overlayer.style.display = "flex";
+}
+
+function closetweet() 
+{
+    const overlayer = document.getElementById("overlay1");
+    overlayer.style.display = "none";  
+}
